63beb4b1882cfed05e28ef9a67dd6860475e02d6
Artem Skaba <73846513+skabartem@users.noreply.github.com>
2021-08-16 21:46:04 +0200

    login/logout/register class based views

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from . import views
+from .views import *
+
 
 urlpatterns = [
-    path('details/', views.profile_details, name='profile-details'),
+    path('details/', ProfileView.as_view(), name='profile-details'),
+
+    path('login/', UserLogin.as_view(), name='login-user'),
+    path('logout/', UserLogout.as_view(), name='logout-user'),
+    path('register/', SignUp.as_view(), name='register-user'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 
-def profile_details(request):
-    context = {}
-    return render(request, 'users/details.html', context)
+class ProfileView(View):
+    template_name = 'users/details.html'
+
+
+class UserLogin(LoginView):
+    template_name = 'users/login.html'
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'users/register.html'
+
+
+class UserLogout(LogoutView):
+    template_name = 'users/details.html'
