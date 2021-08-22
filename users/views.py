0e41c91239af946e51c99af9dd2360a48b0edaa0
skabartem <skabartem@gmail.com>
2021-08-22 19:57:12 +0200

    form field with divs

--- users/views.py
+++ users/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views import generic
 from django.views.generic import CreateView
 from users.forms import SignUpForm
 
@@ -16,15 +13,10 @@ class UserLogin(LoginView):
     template_name = 'users/login.html'
 
 
-# class SignUp(generic.CreateView):
-#     form_class = UserCreationForm
-#     success_url = reverse_lazy('login')
-#     template_name = 'users/register.html'
-
 class SignUp(CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy('login')
-    template_name = 'commons/signup.html'
+    template_name = 'users/register.html'
 
 
 class UserLogout(LogoutView):
