c71694cf3c43eea4c2acf862fb9982e02577560a
skabartem <skabartem@gmail.com>
2021-08-22 19:35:07 +0200

    added sign-up form

--- users/views.py
+++ users/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.views.generic import CreateView
+from users.forms import SignUpForm
 
 
 class ProfileView(View):
@@ -14,10 +16,15 @@ class UserLogin(LoginView):
     template_name = 'users/login.html'
 
 
-class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+# class SignUp(generic.CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'users/register.html'
+
+class SignUp(CreateView):
+    form_class = SignUpForm
     success_url = reverse_lazy('login')
-    template_name = 'users/register.html'
+    template_name = 'commons/signup.html'
 
 
 class UserLogout(LogoutView):
