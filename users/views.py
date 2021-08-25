0cb49f982311044848b96c31a83c1cff9657f618
skabartem <skabartem@gmail.com>
2021-08-25 18:27:16 +0200

    redirect after sucessful login/registration

--- users/views.py
+++ users/views.py
@@ -1,27 +1,29 @@
-from django.views import View
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 from users.forms import SignUpForm, ProfileForm
-from django.contrib.auth.models import User
 from .models import Profile
 
 
 class UserLogin(LoginView):
     template_name = 'users/login.html'
 
+    def get_success_url(self):
+        return reverse_lazy('user-groups')
+
 
 class SignUp(CreateView):
     form_class = SignUpForm
-    success_url = reverse_lazy('login-user')
     template_name = 'users/register.html'
+
+    def get_success_url(self):
+        return reverse_lazy('login-user')
 
 
 # Edit Profile View
 class ProfileView(UpdateView):
     model = Profile
     form_class = ProfileForm
-    success_url = reverse_lazy('login')
     template_name = 'users/details.html'
 
 
