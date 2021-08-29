d795acd58b1d5f180b9df2c6b74face5f8252fc2
skabartem <skabartem@gmail.com>
2021-08-29 22:41:13 +0200

    removed created_expenses from GroupUser model
    the created_expenses field is not required to be present in GroupUser model because it can be querired dirrectly

--- groups/models.py
+++ groups/models.py
@@ -20,7 +20,6 @@ class GroupUser(models.Model):
     balance = models.FloatField(null=True)
     profile = models.ForeignKey(Profile, null=True, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, null=True, on_delete=models.CASCADE)
-    created_expenses = models.ForeignKey("Expense", null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.group} | {self.profile}'
