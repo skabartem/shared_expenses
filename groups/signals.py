b171ecf3ea3f964f9d6607a9c3b42d132e524f64
skabartem <skabartem@gmail.com>
2021-09-08 18:30:34 +0200

    signals code simplification

--- groups/signals.py
+++ groups/signals.py
@@ -1,24 +1,23 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save
 
 from .models import GroupUser, Expense
 
 
 def recalculate_balances_caused_by_expense(sender, instance, created, **kwargs):
+    expense = instance
+    split_with = GroupUser.objects.filter(group=expense.group)
     # update because of added expense
     if not created:
         pass
     # update because of expense update
     elif created:
-        expense = instance
-        split_with = GroupUser.objects.filter(group=expense.group)
         split_amount = expense.price / len(split_with)
         for user in split_with:
             if user != expense.created_by:
                 user.balance = round(user.balance - split_amount, 2)
-                user.save()
             else:
                 user.balance = round(user.balance + split_amount * (len(split_with)-1), 2)
-                user.save()
+            user.save()
 
 
 post_save.connect(recalculate_balances_caused_by_expense, sender=Expense)
