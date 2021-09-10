c453edc03479dad3488bafb1906e0ab107abc2be
skabartem <skabartem@gmail.com>
2021-09-10 21:41:12 +0200

    signals code restrucutre

--- groups/signals.py
+++ groups/signals.py
@@ -1,13 +1,23 @@
 from django.db.models.signals import post_save
-
+from django.dispatch import receiver
 from .models import GroupUser, Expense
 
 
+@receiver(post_save, sender=Expense)
 def recalculate_balances_caused_by_expense(sender, instance, created, **kwargs):
     expense = instance
     split_with = GroupUser.objects.filter(group=expense.group)
     # update caused by added expense
-    if not created:
+    if created:
+        split_amount = expense.price / len(split_with)
+        for user in split_with:
+            if user != expense.created_by:
+                user.balance = round(user.balance - split_amount, 2)
+            else:
+                user.balance = round(user.balance + expense.price - split_amount, 2)
+            user.save()
+    # update caused by expense update
+    else:
         # pool previous expense price
         previous_price = True
         old_split_with = True
@@ -17,15 +27,3 @@ def recalculate_balances_caused_by_expense(sender, instance, created, **kwargs):
             pass
         elif previous_price != expense.price and old_split_with != expense.split_with:
             pass
-    # update caused by expense update
-    elif created:
-        split_amount = expense.price / len(split_with)
-        for user in split_with:
-            if user != expense.created_by:
-                user.balance = round(user.balance - split_amount, 2)
-            else:
-                user.balance = round(user.balance + expense.price - split_amount, 2)
-            user.save()
-
-
-post_save.connect(recalculate_balances_caused_by_expense, sender=Expense)
