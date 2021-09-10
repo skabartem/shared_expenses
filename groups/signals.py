b667cc205b041f85fd15a0db8ca595c86400295d
skabartem <skabartem@gmail.com>
2021-09-10 23:19:44 +0200

    split signals for created and updated expenses
    When expenseis created there should be post_save signal raised. When expense is updated signal should pool old data for comparison first, recalculate balances if needed and only then update the Expense

--- groups/signals.py
+++ groups/signals.py
@@ -1,14 +1,13 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import GroupUser, Expense
 
 
 @receiver(post_save, sender=Expense)
-def recalculate_balances_caused_by_expense(sender, instance, created, **kwargs):
-    expense = instance
-    split_with = GroupUser.objects.filter(group=expense.group)
-    # update caused by added expense
+def recalculate_balances_created_expense(sender, instance, created, **kwargs):
     if created:
+        expense = instance
+        split_with = GroupUser.objects.filter(group=expense.group)
         split_amount = expense.price / len(split_with)
         for user in split_with:
             if user != expense.paid_by:
@@ -16,14 +15,9 @@ def recalculate_balances_caused_by_expense(sender, instance, created, **kwargs):
             else:
                 user.balance = round(user.balance + expense.price - split_amount, 2)
             user.save()
-    # update caused by expense update
-    else:
-        # pool previous expense price
-        previous_price = True
-        old_split_with = True
-        if previous_price != expense.price and old_split_with == expense.split_with:
-            pass
-        elif previous_price == expense.price and old_split_with != expense.split_with:
-            pass
-        elif previous_price != expense.price and old_split_with != expense.split_with:
-            pass
+
+
+@receiver(pre_save, sender=Expense)
+def recalculate_balances_updated_expense(sender, instance, **kwargs):
+    updated_expense = instance
+    prev_expense = Expense.objects.get(id=updated_expense.id)
