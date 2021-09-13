e0476f08c132a1944092104a465ac4a78494c67c
skabartem <skabartem@gmail.com>
2021-09-13 21:20:14 +0200

    not call pre-save when the new expense is created

--- groups/signals.py
+++ groups/signals.py
@@ -19,11 +19,12 @@ def recalculate_balances_created_expense(sender, instance, created, **kwargs):
 
 @receiver(pre_save, sender=Expense)
 def recalculate_balances_updated_expense(sender, instance, **kwargs):
-    updated_expense = instance
-    prev_expense = Expense.objects.get(id=updated_expense.id)
-    if updated_expense.price == prev_expense.price and \
-            updated_expense.paid_by == prev_expense.paid_by and \
-            updated_expense.split_with == prev_expense.split_with:
-        print('NO CHANGES')
-    else:
-        print('RECALCULATION NEEDED')
+    if instance.id is None:
+        updated_expense = instance
+        prev_expense = Expense.objects.get(id=updated_expense.id)
+        if updated_expense.price == prev_expense.price and \
+                updated_expense.paid_by == prev_expense.paid_by and \
+                updated_expense.split_with == prev_expense.split_with:
+            print('NO CHANGES')
+        else:
+            print('RECALCULATION NEEDED')
