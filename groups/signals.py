e98e9fee544d56a625994eeaad9f9402ca2d8756
skabartem <skabartem@gmail.com>
2021-09-15 21:12:05 +0200

    revert previous changes to the group user balance and update the balance according to the changes

--- groups/signals.py
+++ groups/signals.py
@@ -5,27 +5,18 @@ from .models import GroupUser, Expense, ExpenseImpact
 
 @receiver(post_save, sender=Expense)
 def recalculate_balances_created_expense(sender, instance, created, **kwargs):
-    if created:
-        expense = instance
-        split_with = GroupUser.objects.filter(group=expense.group)
-        split_amount = expense.price / len(split_with)
-        for user in split_with:
-            if user != expense.paid_by:
-                ExpenseImpact.objects.create(
-                    group_user=user,
-                    expense=expense,
-                    balance_impact=-split_amount,
-                )
-                user.balance = round(user.balance - split_amount, 2)
-            else:
-                lent = expense.price - split_amount
-                ExpenseImpact.objects.create(
-                    group_user=user,
-                    expense=expense,
-                    balance_impact=lent,
-                )
-                user.balance = round(user.balance + lent, 2)
-            user.save()
+    expense = instance
+    split_with = GroupUser.objects.filter(group=expense.group)
+    split_amount = expense.price / len(split_with)
+    for user in split_with:
+        if user != expense.paid_by:
+            ExpenseImpact.objects.create(group_user=user, expense=expense, balance_impact=-split_amount)
+            user.balance = round(user.balance - split_amount, 2)
+        else:
+            lent = expense.price - split_amount
+            ExpenseImpact.objects.create(group_user=user, expense=expense, balance_impact=lent)
+            user.balance = round(user.balance + lent, 2)
+        user.save()
 
 
 @receiver(pre_save, sender=Expense)
@@ -38,7 +29,11 @@ def recalculate_balances_updated_expense(sender, instance, **kwargs):
         same_lander = updated_expense.paid_by == prev_expense.paid_by
         same_borrowers = updated_expense.split_with == prev_expense.split_with
 
-        if same_price and same_lander and same_borrowers:
-            print('NO CHANGES')
-        else:
-            print('RECALCULATION NEEDED')
+        if not same_price or not same_lander or not same_borrowers:
+            impacted_users = ExpenseImpact.objects.filter(expense=prev_expense)
+
+            # revert balance changes caused by the expense
+            for impact in impacted_users:
+                impact.group_user.balance = impact.group_user.balance - impact.balance_impact
+                impact.group_user.save()
+                impact.delete()
