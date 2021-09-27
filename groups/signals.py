00646842c5a75c8b6779260e08e67830a5de882c
skabartem <skabartem@gmail.com>
2021-09-27 23:47:38 +0200

    code readability improvement

--- groups/signals.py
+++ groups/signals.py
@@ -11,35 +11,9 @@ def manage_transfers(group):
     min_cash_flow_rec(grp_users, balances)
 
 
-@receiver(post_save, sender=Expense)
-def recalculate_balances_created_expense(sender, instance, created, **kwargs):
-    if created:
-        expense = instance
-        split_with = GroupUser.objects.filter(group=expense.group)
-        split_amount = round(expense.price / len(split_with), 2)
-
-        lent = expense.price - split_amount if expense.paid_by in split_with else expense.price
-        rounding_fix = round(split_amount + 0.01, 2) if expense.price % split_amount else 0
-
-        for user in split_with:
-            if rounding_fix and user != expense.paid_by:
-                user.balance += -rounding_fix
-                CashMovement.objects.create(group_user=user, expense=expense, balance_impact=rounding_fix)
-                rounding_fix = 0
-            elif user != expense.paid_by:
-                user.balance += -split_amount
-                CashMovement.objects.create(group_user=user, expense=expense, balance_impact=split_amount)
-            user.save()
-
-        expense.paid_by.balance += lent
-        CashMovement.objects.create(group_user=expense.paid_by, expense=expense, balance_impact=lent)
-        expense.paid_by.save()
-
-        manage_transfers(expense.group)
-
-
+# signal CALLED BEFORE updated expense data pushed to DB
 @receiver(pre_save, sender=Expense)
-def recalculate_balances_updated_expense(sender, instance, **kwargs):
+def revert_previous_expense_changes(sender, instance, **kwargs):
     if not instance._state.adding:
         updated_expense = instance
         prev_expense = Expense.objects.get(id=updated_expense.id)
@@ -49,10 +23,38 @@ def recalculate_balances_updated_expense(sender, instance, **kwargs):
         same_borrowers = updated_expense.split_with == prev_expense.split_with
 
         if not same_price or not same_lander or not same_borrowers:
-            impacted_users = CashMovement.objects.filter(expense=prev_expense)
+            cash_movements = CashMovement.objects.filter(expense=prev_expense)
 
             # revert balance changes caused by the expense
-            for impact in impacted_users:
-                impact.group_user.balance = impact.group_user.balance - impact.balance_impact
-                impact.group_user.save()
-                impact.delete()
+            for balance_change in cash_movements:
+                balance_change.group_user.balance = balance_change.group_user.balance - balance_change.balance_impact
+                balance_change.group_user.save()
+                balance_change.delete()
+
+
+# signal CALLED AFTER updated expense data pushed to DB
+@receiver(post_save, sender=Expense)
+def recalculate_group_balances(sender, instance, created, **kwargs):
+    expense = instance
+    split_with = GroupUser.objects.filter(group=expense.group)
+    split_amount = round(expense.price / len(split_with), 2)
+
+    lent = expense.price - split_amount if expense.paid_by in split_with else expense.price
+    rounding_fix = round(split_amount + 0.01, 2) if expense.price % split_amount else 0
+
+    for user in split_with:
+        if rounding_fix and user != expense.paid_by:
+            user.balance += -rounding_fix
+            CashMovement.objects.create(group_user=user, expense=expense, balance_impact=-rounding_fix)
+            rounding_fix = 0
+        elif user != expense.paid_by:
+            user.balance += -split_amount
+            CashMovement.objects.create(group_user=user, expense=expense, balance_impact=-split_amount)
+        user.save()
+
+    lender = GroupUser.objects.get(id=expense.paid_by.id)
+    lender.balance += lent
+    CashMovement.objects.create(group_user=expense.paid_by, expense=expense, balance_impact=lent)
+    lender.save()
+
+    manage_transfers(expense.group)
