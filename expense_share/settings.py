0d2b6631ba5ec4f1f664b20d8474cb3d9cb49073
skabartem <skabartem@gmail.com>
2021-10-05 21:52:18 +0200

    add static file dir to settings

--- expense_share/settings.py
+++ expense_share/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFIlES_DIRS = [
+    Path.joinpath(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
