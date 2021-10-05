c6b7841b8d17fd25761d12598a9f7e9e85eb2360
skabartem <skabartem@gmail.com>
2021-10-05 21:56:10 +0200

    add media root to settings

--- expense_share/settings.py
+++ expense_share/settings.py
@@ -122,10 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
-STATICFIlES_DIRS = [
+STATICFILES_DIRS = [
     Path.joinpath(BASE_DIR, 'static'),
 ]
+
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'static/images')
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
