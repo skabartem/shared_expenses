f2e6c26b1dfa386fc9ce8ea1eef3548fc7485786
skabartem <skabartem@gmail.com>
2021-10-19 21:51:02 +0200

    make pagination work

--- groups/views.py
+++ groups/views.py
@@ -17,6 +17,8 @@ from .utils import track_cash_movements
 
 from datetime import datetime
 
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 
 @method_decorator(login_required, name='dispatch')
 class GroupDetailView(DetailView):
@@ -37,9 +39,7 @@ class GroupDetailView(DetailView):
         context = super().get_context_data(**kwargs)
 
         group = Group.objects.get(id=self.kwargs['pk'])
-
         context['logged_user'] = self.request.user.profile
-        context['group_expenses'] = Expense.objects.filter(group=group).order_by('-paid_date')
         context['group_users'] = GroupUser.objects.filter(group=group)
         context['cash_transfers'] = TransferToMake.objects.filter(group=group)
         context['group_data'] = group
@@ -47,6 +47,35 @@ class GroupDetailView(DetailView):
         user_groups = Group.objects.filter(profile=self.request.user.profile).order_by('-last_update')
         context['current_group_id'] = group.id
         context['user_groups'] = user_groups
+
+        # pagination
+        expenses = Expense.objects.filter(group=group).order_by('-paid_date')
+
+        page = self.request.GET.get('page')
+        el_per_page = 11
+        left_right_pages_range = 2
+        paginator = Paginator(expenses, el_per_page)
+
+        try:
+            expenses = paginator.page(page)
+        except PageNotAnInteger:
+            page = 1
+            expenses = paginator.page(page)
+        except EmptyPage:
+            page = paginator.num_pages
+            expenses = paginator.page(page)
+
+        left_index = int(page) - left_right_pages_range
+        if left_index < 1:
+            left_index = 1
+        right_index = int(page) + left_right_pages_range + 1
+        if right_index > paginator.num_pages:
+            right_index = paginator.num_pages + 1
+
+        page_range = range(left_index, right_index)
+
+        context['group_expenses'] = expenses
+        context['custom_range'] = page_range
 
         self.request.session['group_id'] = str(group.id)
         return context
