ce812a9bee92f38f5d578fbf481a321dfababf21
skabartem <skabartem@gmail.com>
2021-11-07 21:46:56 +0100

    add user details to profile update page

--- users/views.py
+++ users/views.py
@@ -67,3 +67,8 @@ class ProfileView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('user-groups')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['logged_user'] = self.request.user.profile
+        return context
